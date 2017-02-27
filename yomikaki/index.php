c894d6415a56a2a5d340b897a531b1d45b8d83d1
Mist <JUNJI_YOSHIDA@yoshidajunji-no-MacBook-Pro.local>
2017-02-27 12:10:08 +0900

    テキストファイルに書き込むサンプル
    テキストファイルに書き込むサンプル

---
+++ yomikaki/index.php
@@ -0,0 +1,10 @@
+<?php
+//ファイルパス設定
+$target_dir = dirname(__FILE__);
+$target_file = $target_dir . "/test.txt";
+//ファイルに文字列を書き込む
+file_put_contents($target_file, "アイウエオ");
+//ふぁいるから文字列を読み込む
+$str = file_get_contents($target_file);
+echo "読み書きした文字列： $str";
+?>
