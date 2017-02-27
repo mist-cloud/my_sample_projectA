2a3ceaf1dc70b45e0e314b48b4dc3bc29aa901ae
Mist <JUNJI_YOSHIDA@yoshidajunji-no-MacBook-Pro.local>
2017-02-27 18:24:57 +0900

    PHPの練習　一行掲示板など

---
+++ save-array-ok.php
@@ -0,0 +1,10 @@
+<?php
+//配列をファイルに保存
+$list = array("Apple"=>300, "Banana"=>130, "Orange"=>200);
+$savedata = serialize($list); //シアライズ
+file_put_contents("fruits.txt", $savedata);
+//保存したデータを読み込んで表示
+$loaddata = file_get_contents("fruits.txt");
+$list = unserialize($loaddata); //シリアライズされたデータをPHPのデータに戻す。
+echo $list["Banana"]; //→正しく（130）になる
+?>
---
+++ test-salt.php
@@ -0,0 +1,7 @@
+<?php
+$password = "test";
+$salt = "bnr4u9hx6ks4rdp1ded"; //類推されにくい文字列を指定
+$pass_with_salt = $password.$salt;
+$hash = hash("sha512", $pass_with_salt);
+echo $hash;
+?>
