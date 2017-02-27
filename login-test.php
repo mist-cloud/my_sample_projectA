4fe375983b36f97eaedb82810380fea822f40854
Mist <JUNJI_YOSHIDA@yoshidajunji-no-MacBook-Pro.local>
2017-02-27 20:24:57 +0900

    PHPでのログイン画面実装
    PHPでのログイン画面実装

---
+++ login-test.php
@@ -0,0 +1,63 @@
+<?php
+//ユーザーとパスワードの一覧
+$users = array(
+    //ユーザー名=>パスワード（SHA1でハッシュ化）
+    "takeshi"=>"a94a8fe5ccb19ba61c4c0873d391e987982fbbd3",//test
+    "yutaka"=>"ee3f7d52ca341c51c694af9288701f4ce43be0ad",//rabit
+    "akiko"=>"a9c1b6a22aa74abcd4fad7fbc3b0177c25efbd77",//panda
+);
+$script = $_SERVER["SCRIPT_NAME"]; //このPHPファイルのパス
+//セッションを開始する
+session_start();
+//ログインしているか？
+if (isset($_SESSION["login"])) {
+    show_login_contents(); exit;
+}
+//ログインフォームからの入力はあるか？
+if (isset($_POST["user"])) {
+    check_login();//ログインできるかチェック
+} else {
+    show_login_form();//ログインフォームを表示
+}
+//ログインフォームを表示
+function show_login_form() {
+    global $script;
+    echo <<< __FORM__
+<div id="loginform">
+<form action="$script" method="POST">
+<h3>ログインしてください</h3>
+<label>ユーザー名</label>
+<input type="text" name="user" />
+<input type="password" name="pass" />
+<button type="submit">ログイン</button>
+</form>
+</div>
+</div>
+__FORM__;
+}
+//ログインするかチェック
+function check_login() {
+    global $users, $script;
+    //入力を検証する
+    if (empty($_POST["pass"])) {
+        echo "パスワードが入力されていません。"; exit;
+    }
+    if (empty($users[$_POST["user"]])) {
+        echo "ユーザーが存在しないかパスワードが違います。"; exit;
+    }
+    //パスワードが合致しているかチェック
+    $pass_correct = $users[$_POST["user"]];
+    if (sha1($_POST["pass"]) != $pass_correct) {
+        echo "ユーザーが存在しないかパスワードが違います。"; exit;
+    }
+    //ログインしたことをセッションに記録
+    $_SESSION["login"] = array("user" => $_POST["user"]);
+    echo "<a href='$script'>ログインしました</a>";
+}
+//ログインしている時の処理
+function show_login_contents() {
+    $user = $_SESSION["login"]["user"];
+    echo "<h1>こんにちは、{$user}さん</h1>";
+    echo "<p>このページはログインした人にだけ見えるページです。</p>";
+}
+?>
--- test-salt.php
+++ test-salt.php
@@ -2,6 +2,6 @@
 $password = "test";
 $salt = "bnr4u9hx6ks4rdp1ded"; //類推されにくい文字列を指定
 $pass_with_salt = $password.$salt;
-$hash = hash("sha512", $pass_with_salt);
+$hash = hash("sha1", $pass_with_salt);
 echo $hash;
 ?>
