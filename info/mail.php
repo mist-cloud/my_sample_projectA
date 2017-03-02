5391b563cd3a01b9367793aaf2ce3eddbee72c6e
Mist <JUNJI_YOSHIDA@yoshidajunji-no-MacBook-Pro.local>
2017-03-02 18:52:48 +0900

    お問い合わせフォームをPHPで作成
    一応動く。

--- info/mail.php
+++ info/mail.php
@@ -3,35 +3,29 @@
 mb_language("japanese");
 mb_internal_encoding("utf-8");
   
-if(!empty($_POST['mail'])){
+if(!empty($_POST['mail']) && ($_POST['naiyou']) && ($_POST['sub'])){
     $name=$_POST['name'];//お名前
     $gender=$_POST['gender'];//性別
     $blood=$_POST['blood'];//血液型
     $yubin=$_POST['yubin'];//郵便番号
-    $to=$_POST['mail'];//メールアドレス
     $subject=$_POST['sub'];//件名
     $naiyou=$_POST['naiyou'];//お問い合わせ内容
+    $mymail="256hunter@gmail.com";//【　送信先メールアドレスを指定する　】
 
-//郵便番号の型の判定
-$yubin=mb_convert_kana($_POST['yubin'],'a','utf-8');
-  
-if(!preg_match("/\A\d{3}\-\d{4}\z/",$yubin)){
-    $yubin="正しい郵便番号を入力してください";
-}
-//ここまで郵便番号
+//送信されたメールを読みやすいように整理する
+$naiyouall="お名前：".$name."\n性別：".$gender."\n血液型：".$blood."\n郵便番号：".$yubin."\nお問い合わせ内容：".$naiyou;
 
 //差出人の名前を表示
 $mail=$_POST['mail'];    
 $from=mb_encode_mimeheader(mb_convert_encoding($name,"jis","utf-8"))."<".$mail.">";
 
-$success=mb_send_mail($to,$subject,"名前：".$name."　性別：".$gender."　血液型：".$blood."　郵便番号：".$yubin."　本文：".$naiyou,"from:".$from);
+//送信処理
+$success=mb_send_mail($mymail,$subject,$naiyouall,"From:".$from);//mb_send_mail(送信先アドレス,題名,本文,送信元アドレス)
 }
-?>
-  
-  
-<?php 
+
 if($success){
-    echo('送信しました');
+    echo('送信しました<br /><br />');
+    echo('<a href="index.php">戻る</a>');
 }else{
     echo('送信失敗！！');
 }
