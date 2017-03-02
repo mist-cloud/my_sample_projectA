e1c301931fa4442b3b51ddd4ece10bc076a39307
Mist <JUNJI_YOSHIDA@yoshidajunji-no-MacBook-Pro.local>
2017-03-02 19:24:01 +0900

    お問い合わせフォームをPHPで作成　追加修正

--- info/index.php
+++ info/index.php
@@ -4,6 +4,11 @@
   <meta charset="UTF-8">
   <title>Document</title>
   <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+<style type="text/css">
+
+</style>
+
 </head>
 <body>
   <div id="page">
@@ -13,20 +18,20 @@
             <form method="post" action="mail.php" class="form-horizontal">
             
             <div class="form-group">
-                <label for="" class="col-sm-2 control-label">お名前</label>
-                <div class="col-sm-10">
-                  <input name="name" id="name" type="text" size="50" />
+                <label for="" class="col-sm-2 col-lg-3 control-label">お名前</label>
+                <div class="col-sm-10 col-lg-9">
+                  <input name="name" id="name" type="text" size="50" class="form-control" />
                 </div>
             </div>
             <div class="form-group">
-                <label for="" class="col-sm-2 control-label">性別</label>
-                <div class="col-sm-10">
+                <label for="" class="col-sm-2 col-lg-3 control-label">性別</label>
+                <div class="col-sm-10 col-lg-9">
                     <input name="gender" id="man" type="radio" value="男性" /><label for="man">男性</label>　<input name="gender" id="woman" type="radio" value="女性" /><label for="woman">女性</label>
                 </div>
             </div>
             <div class="form-group">
-                <label for="" class="col-sm-2 control-label">血液型</label>
-                <div class="col-sm-10">
+                <label for="" class="col-sm-2 col-lg-3 control-label">血液型</label>
+                <div class="col-sm-10 col-lg-9">
                     <select name="blood" id="blood">
                     <option value="A型">A型</option>
                     <option value="B型">B型</option>
@@ -36,31 +41,31 @@
                 </div>
             </div>
             <div class="form-group">
-                <label for="" class="col-sm-2 control-label">郵便番号</label>
-                <div class="col-sm-10">
-                    <input name="yubin" id="yubin" type="text" size="50" />
+                <label for="" class="col-sm-2 col-lg-3 control-label">郵便番号</label>
+                <div class="col-sm-10 col-lg-9">
+                    <input name="yubin" id="yubin" type="text" size="50" class="form-control" />
                 </div>
             </div>
             <div class="form-group">
-                <label for="" class="col-sm-2 control-label">メールアドレス</label>
-                <div class="col-sm-10">
-                  <input name="mail" id="mail" type="text" size="50" maxlength="255" />
+                <label for="" class="col-sm-2 col-lg-3 control-label">メールアドレス</label>
+                <div class="col-sm-10 col-lg-9">
+                  <input name="mail" id="mail" type="text" size="50" maxlength="255" class="form-control" />
                 </div>
             </div>
             <div class="form-group">
-                <label for="" class="col-sm-2 control-label">件名</label>
-                <div class="col-sm-10">
-                    <input name="sub" id="sub" type="text" size="50" maxlength="255" />
+                <label for="" class="col-sm-2 col-lg-3 control-label">件名</label>
+                <div class="col-sm-10 col-lg-9">
+                    <input name="sub" id="sub" type="text" size="50" maxlength="255" class="form-control" />
                 </div>
             </div>
             <div class="form-group">
-                <label for="" class="col-sm-2 control-label">お問い合わせ内容</label>
-                <div class="col-sm-10">
-                    <textarea name="naiyou" id="naiyou" cols="50" rows="10"></textarea>
+                <label for="" class="col-sm-2 col-lg-3 control-label">お問い合わせ内容</label>
+                <div class="col-sm-10 col-lg-9">
+                    <textarea name="naiyou" id="naiyou" cols="50" rows="10" class="form-control"></textarea>
                 </div>
             </div>
             <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-10">
+            <div class="col-sm-offset-2 col-sm-10 col-lg-9">
               <button type="submit" class="btn btn-default">送信</button>
             </div>
             </div>
