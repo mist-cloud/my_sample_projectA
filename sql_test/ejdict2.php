acc93be484a98606dbd56a78cf25ae3720fc87cf
Mist <JUNJI_YOSHIDA@yoshidajunji-no-MacBook-Pro.local>
2017-03-02 12:54:46 +0900

    SQLiteの英和辞書にタイトル＋本文検索機能を追加
    SQLiteの英和辞書にタイトル＋本文検索機能を追加

--- sql_test/ejdict2.php
+++ sql_test/ejdict2.php
@@ -14,11 +14,18 @@ echo <<< __FORM__
 __FORM__;
 //検索するか？
 if ($title != "") {
-    //$stmt = $db->prepare("SELECT * FROM words WHERE title=?");//完全一致
-    //$stmt->execute(array($title));//完全一致
+    ///英語のタイトルから検索
     $stmt = $db->prepare("SELECT * FROM words WHERE title LIKE ? LIMIT 20");
     $stmt->execute(array($title."%"));
-    foreach ($stmt as $row) {
+    $rows = $stmt->fetchAll();//配列で検索結果を全部取得する
+    //検索した結果が0だったら本文を検索
+    if (count($rows) == 0) {
+        $stmt = $db->prepare("SELECT * FROM words WHERE body LIKE ? LIMIT 20");
+        $stmt->execute(array("%{$title}%"));
+        $rows = $stmt->fetchAll();//配列で検索結果を全部取得する
+    }
+    //検索結果を表示
+    foreach ($rows as $row) {
         $word = htmlspecialchars($row["title"]);
         $body = str_replace(" / ", "\n", $row["body"]);
         $body = htmlspecialchars($body);
